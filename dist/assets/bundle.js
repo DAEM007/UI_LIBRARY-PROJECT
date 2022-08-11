d6cd726c3e3a83ee0b79ca8cbf1c693eb8119c12
EMMANUEL <dammylola299@gmail.com>
2022-08-11 19:30:21 +0100

    Finally figured it out---can now refresh browser automatically

--- dist/assets/bundle.js
+++ dist/assets/bundle.js
@@ -1,9 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 /******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log('Hey');
+/***/ (() => {
+
+eval("console.log('Hello flag!');\n\n//# sourceURL=webpack://ui_library-project/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLEtBQVosRSIsInNvdXJjZXMiOlsid2VicGFjazovL3VpX2xpYnJhcnktcHJvamVjdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGV5Jyk7Il0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9
--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,19 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/',
+    filename: 'bundle.js',
+    clean: true,
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
+      watch: true,
     },
     compress: true,
     port: 9000,
-    watchOptions: {
-      poll: true
-    }
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/assets/'),
-    filename: 'bundle.js',
   },
   module: {
     rules: [{
