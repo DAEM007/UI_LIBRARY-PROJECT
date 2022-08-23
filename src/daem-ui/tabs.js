d6314cfce3451c2b17b2f00df1f0222c8bd6fc21
EMMANUEL <dammylola299@gmail.com>
2022-08-23 15:49:37 +0100

    Done with Tabs feature in Tabs.js

--- src/daem-ui/tabs.js
+++ src/daem-ui/tabs.js
@@ -2,6 +2,7 @@ class Tabs {
     constructor(container){
         this.container = container;
         this.tabs = container.querySelectorAll('.trigger');
+        this.content = container.querySelectorAll('.content');
     }
     init(){
         this.tabs.forEach((tab) => {
@@ -11,4 +12,24 @@ class Tabs {
             })
         })
     }
+    toggleTabs(e){
+        // Remove current active class
+        this.tabs.forEach((tab) => {
+            tab.classList.remove('active');
+        })
+        // Add new active class for the clicked tab
+        e.target.classList.add('active');
+    }
+    toggleContent(e){
+        // Remove current active class
+        this.content.forEach((item) => {
+            item.classList.remove('active');
+        })
+        // Add new active class to the clicked tab
+        const selector = e.target.getAttribute('data-target');
+        const content = this.container.querySelector(selector);
+        content.classList.add('active');
+    }
 }
+
+export { Tabs as default };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-// import '../src/daem-ui/styles/tooltip.css';
+//ALL IMPORTS
+//  import '../src/daem-ui/styles/tooltip.css';
 import Tooltip from './daem-ui/tooltip';
 import Dropdown from './daem-ui/dropdown';
+import Tabs from './daem-ui/tabs';
 
 // QUERIES
 const Span = document.querySelector('.tooltip');
 const dropDowns = document.querySelectorAll('.drop-down');
 
-// CREATE A TOOLTIP AND INITIALIZE
+// Create an instance of class Tooltip and initialize
 const toolTip = new Tooltip(Span);
 toolTip.init();
 
-// CREATE A DROP-DOWN AND INITIALIZE
+// Create an instance of class Dropdown and initialize
 dropDowns.forEach(dropDown => {
     const instance = new Dropdown(dropDown);
     instance.init();
 });
 
+// Create an instance of class Tab initialize
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
